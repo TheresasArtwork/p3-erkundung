52cff69fe9cf01225a6f1dda500a8c845a667307
amysutcliffe <amybsutcliffe@icloud.com>
2024-02-01 10:36:41 +0100

    New map style

--- src/mapbox/Map.jsx
+++ src/mapbox/Map.jsx
@@ -17,7 +17,7 @@ export function Map() {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v12",
+      style: "mapbox://styles/mapbox/light-v11",
       center: [lng, lat],
       zoom: zoom,
     });

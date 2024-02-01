28b6f78b9091dad2fb31efd51831fa654bf81a4b
Shekinah Gitumbu <shekinah.gitumbu@stud.h-da.de>
2024-02-01 11:53:36 +0100

    adding first markers

--- src/mapbox/Map.jsx
+++ src/mapbox/Map.jsx
@@ -1,39 +1,67 @@
-import { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
+import { createRoot } from 'react-dom/client';
 import "./Map.css";
 import "../../config.js";
-import mapboxgl, { accessToken } from "mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import mapboxgl from "mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 import { accessTokenMapbox } from "../../config.js";
+import geoJson from '../mapbox-marker/GeoJSON.json';
+
 
 mapboxgl.accessToken = accessTokenMapbox;
 
-export function Map() {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(8.8445);
-  const [lat, setLat] = useState(49.9011);
-  const [zoom, setZoom] = useState(15);
+const Marker = ({ onClick, children, feature }) => {
+  const _onClick = () => {
+    onClick(feature.properties.description);
+  };
 
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/light-v11",
-      center: [lng, lat],
-      zoom: zoom,
-    });
-
-    map.current.on("move", () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-  });
   return (
-    <>
-      <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div>
-      <div ref={mapContainer} className="map-container" />
-    </>
+    <button onClick={_onClick} className="marker">
+      {children}
+    </button>
   );
-}
+};
+
+const Map = () => {
+  const mapContainerRef = useRef(null);
+
+  // Initialize map when component mounts
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [8, 49],
+      zoom: 15,
+    });
+
+    // Render custom marker components
+    geoJson.features.forEach((feature) => {
+      // Create a React ref
+      const ref = React.createRef();
+      // Create a new DOM node and save it to the React ref
+      ref.current = document.createElement('div');
+      // Render a Marker Component on our new DOM node
+      createRoot(ref.current).render(
+        <Marker onClick={markerClicked} feature={feature} />
+      );
+
+      // Create a Mapbox Marker at our new DOM node
+      new mapboxgl.Marker(ref.current)
+        .setLngLat(feature.geometry.coordinates)
+        .addTo(map);
+    });
+
+    // Add navigation control (the +/- zoom buttons)
+    map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
+    // Clean up on unmount
+    return () => map.remove();
+  }, []);
+
+  const markerClicked = (title) => {
+    window.alert(title);
+  };
+
+  return <div className="map-container" ref={mapContainerRef} />;
+};
+
+export default Map;

3a8af3b48e52dbb1c95cd36879fd07c8e8dc4e7e
Theresa Husske <theresa.husske@t-online.de>
2024-02-02 21:26:45 +0100

    added an active statement

--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -1,20 +1,42 @@
+import { Link, Outlet, useLocation } from "react-router-dom";
 import "./Menu.css";
-import { Link, Outlet } from "react-router-dom";
 
 export function Menu() {
+  const location = useLocation();
+
   return (
     <>
       <div className="menu-wrap">
-        <Link to="/trekgo" className="menu-link">
+        <Link
+          to="/trekgo"
+          className={`menu-link ${
+            location.pathname === "/trekgo" ? "active" : ""
+          }`}
+        >
           TrekGo
         </Link>
-        <Link to="/cultivate" className="menu-link">
+        <Link
+          to="/cultivate"
+          className={`menu-link ${
+            location.pathname === "/cultivate" ? "active" : ""
+          }`}
+        >
           Cultivate
         </Link>
-        <Link to="/cinetrail" className="menu-link">
+        <Link
+          to="/cinetrail"
+          className={`menu-link ${
+            location.pathname === "/cinetrail" ? "active" : ""
+          }`}
+        >
           CineTrail
         </Link>
-        <Link to="/kidquest" className="menu-link">
+        <Link
+          to="/kidquest"
+          className={`menu-link ${
+            location.pathname === "/kidquest" ? "active" : ""
+          }`}
+        >
           KidQuest
         </Link>
       </div>

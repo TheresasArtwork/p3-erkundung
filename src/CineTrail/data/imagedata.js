60feb980f6262cfde0fd8d75d06867a08c5b7291
darisavchenko <daria.savchenko@stud.h-da.de>
2024-02-08 16:36:16 +0100

    CineTrail image url
    works without http://localhost:5173

--- src/CineTrail/data/imagedata.js
+++ src/CineTrail/data/imagedata.js
@@ -2,19 +2,19 @@ export const imageData = {
   location1: [
     {
       title: "Arrival to the store",
-      url: "http://localhost:5173/images/liberty-arrival-to-the-store.jpg",
+      url: "/images/liberty-arrival-to-the-store.jpg",
       clue1: "Clue 1",
       clue2: "Clue 2",
     },
     {
       title: "Annoying manager",
-      url: "http://localhost:5173/images/liberty-annoying-manager.jpg",
+      url: "/images/liberty-annoying-manager.jpg",
       clue1: "Look for the jewelry stand",
       clue2: "Search for the Staff Only door",
     },
     {
       title: "The store window",
-      url: "http://localhost:5173/images/liberty-the-store-window.jpg",
+      url: "/images/liberty-the-store-window.jpg",
       clue1: "Look for the window on the right near the entrance",
       clue2: "Clue 2",
     },
@@ -22,13 +22,13 @@ export const imageData = {
   location2: [
     {
       title: "Entrance",
-      url: "http://localhost:5173/images/hell-hall-entrance.jpg",
+      url: "/images/hell-hall-entrance.jpg",
       clue1: "Clue 1",
       clue2: "Clue 2",
     },
     {
       title: "The Fall",
-      url: "http://localhost:5173/images/hell-hall-the-fall.jpg",
+      url: "/images/hell-hall-the-fall.jpg",
       clue1: "Clue 1",
       clue2: "Clue 2",
     },

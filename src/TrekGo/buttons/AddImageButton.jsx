c92b21c9b2699024c0b6252947e2e7b5df7cf313
Theresa Husske <theresa.husske@t-online.de>
2024-02-09 14:09:17 +0100

    added Buttons

--- src/TrekGo/buttons/AddImageButton.jsx
+++ src/TrekGo/buttons/AddImageButton.jsx
@@ -1,3 +1,5 @@
+import "./Button.css";
+
 export function AddImageButton() {
   return (
     <>
---
+++ src/TrekGo/buttons/BackButton.jsx
@@ -0,0 +1,11 @@
+import "../buttons/Button.css";
+
+export function BackButton() {
+  return (
+    <>
+      <button className="image-button" id="back-button">
+        <img src="src\TrekGo\icons\backArrow.svg"></img>
+      </button>
+    </>
+  );
+}

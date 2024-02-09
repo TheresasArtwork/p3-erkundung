844459349202fbecdf861732e3e70f3a3a273a39
Theresa Husske <theresa.husske@t-online.de>
2024-02-09 14:09:35 +0100

    added an Popup Componente

---
+++ src/TrekGo/PopUpLayer/PopupTrekGo.jsx
@@ -0,0 +1,14 @@
+import Header from "../Header/Header";
+import "../PopUpLayer/PopupTrekGo.css";
+
+const PopupTrekGo = () => {
+  return (
+    <>
+      <div className="popup-trekgo">
+        <Header></Header>
+      </div>
+    </>
+  );
+};
+
+export default PopupTrekGo;

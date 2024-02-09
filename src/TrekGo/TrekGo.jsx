545456c62de1168652714a2a5c6dcccbd3e21c3a
Theresa Husske <theresa.husske@t-online.de>
2024-02-09 14:10:12 +0100

    updated TrekGo Componente

--- src/TrekGo/TrekGo.jsx
+++ src/TrekGo/TrekGo.jsx
@@ -1,9 +1,23 @@
-import { AddImageButton } from "./AddImageButton.jsx";
+// import { AddImageButton } from "./buttons/AddImageButton.jsx";
+import MapTrekGo from "./Map/MapTrekGo.jsx";
+import PopupTrekGo from "./PopUpLayer/PopupTrekGo.jsx";
+import geoJson from "../mapbox-marker/GeoJSON.json";
 
-export function TrekGo() {
+import { useState } from "react";
+
+const TrekGo = () => {
+  const [popupTitle, setPopupTitle] = useState("");
+
+  const handleMarkerClick = () => {
+    setPopupTitle("Neuer Popup-Titel");
+  };
+
   return (
-    <>
-      <AddImageButton></AddImageButton>
-    </>
+    <div className="parent-container">
+      <MapTrekGo geoJson={geoJson} onMarkerClick={handleMarkerClick} />
+      {popupTitle && <PopupTrekGo title={popupTitle} />}
+    </div>
   );
-}
+};
+
+export default TrekGo;
